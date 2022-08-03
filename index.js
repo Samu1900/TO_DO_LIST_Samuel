8cbb29251c66cd7107166ac825315ef2f93162cd
Samu1900 <Samucaalonso655@gmail.com>
2022-08-03 10:28:02 -0300

    Atualização 2

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require("express");
+const path = require("path");
+const routes = require("./routes/routes");
+
 const app = express();
+const port = 3000;
 
-app.get("/home", (req, res) => {
-  res.render("Hello Word");
-});
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+app.use(routes);
 
-app.listen(3000, () => {
-  console.log("Conectado!");
-});
+//ROTA
+
+app.listen(port, () =>
+  console.log(`Servidor rodando em http://localhost:${port}`)
+);
